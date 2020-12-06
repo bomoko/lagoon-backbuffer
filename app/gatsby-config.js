90dce862e441e71602a3764e5dd06472771af9c7
Blaize Kaye <blaize.kaye@amazee.com>
2020-12-06 19:18:27 +1300

    Adds graphql integration for demo

--- app/gatsby-config.js
+++ app/gatsby-config.js
@@ -6,5 +6,18 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+        // Simple config, passing URL
+        {
+          resolve: "gatsby-source-graphql",
+          options: {
+            // Arbitrary name for the remote schema Query type
+            typeName: "WEATHER",
+            // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
+            fieldName: "weather",
+            // Url to query from
+            url: "https://graphql-weather-api.herokuapp.com/"
+          }
+        }
+  ]
 }
--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,5 +1,22 @@
 import React from "react"
 
-export default function Home() {
-  return <div>Hello world! from the back buffer</div>
+export default function Home({ data }) {
+  let feltLike = data.weather.getCityByName.weather.temperature.feelsLike
+  let actuallyIs = data.weather.getCityByName.weather.temperature.actual
+  return <div>Hello world - from Wellington, where, at build time the temperature felt like {feltLike} but was actually {actuallyIs}</div>
 }
+
+
+export const query = graphql`
+  query {
+    weather { getCityByName(name:"Wellington") {
+      weather {
+        temperature {
+          feelsLike
+          actual
+        }
+      }
+    }
+    }
+  }
+`
